57e30f1b808e001d8a3371213aa85759972e40b6
Alexey Fedorov <afedorov@perfectpanel.com>
2022-09-08 18:33:53 +0300

    [MY-3548] - filters template

--- modules/OrdersList/views/orders/index.php
+++ modules/OrdersList/views/orders/index.php
@@ -35,7 +35,41 @@ $this->params['breadcrumbs'][] = $this->title;
 -->
 </nav>
 <div class="container-fluid">
-
+    <ul class="nav nav-tabs p-b">
+        <li <?= !isset($param['status']) ? 'class="active"' : '' ?>>
+            <?= Html::a(Yii::t('common', 'All orders'), ['/orders']) ?>
+        </li>
+        <?php foreach ($statuses as $status_id => $status) : ?>
+            <li <?= isset($param['status']) ? ($param['status'] ==  $status_id ? 'class="active"' : '' ) : '' ?> >
+                <?= Html::a($status, ['/orders', 'status' => $status_id]) ?>
+            </li>
+        <?php endforeach; ?>
+        <li class="pull-right custom-search">
+            <?php $form = ActiveForm::begin([
+                'method' => 'get',
+                'action' => ['/orders'],
+                'options' => ['class' => 'form-inline'],
+            ]) ?>
+            <div class="input-group">
+                <?= isset($param['status'])
+                    ? Html::tag('input', '', ['type' => 'hidden', 'name' => 'status', 'value' => $param['status']])
+                    : ''  ?>
+                <?=Html::tag('input','',['type' => 'text',
+                    'name' => 'search',
+                    'class' => 'form-control',
+                    'placeholder' =>  Yii::t('common', 'Search orders'),
+                    'value' => isset($param['search']) ? $param['search'] : ''
+                ]) ?>
+                <span class="input-group-btn search-select-wrap">
+            <?= Html::dropDownList('searchType', isset($param['searchType']) ? $param['searchType'] : 1, $search, ['class' => 'form-control search-select']) ?>
+            <button type="submit" class="btn btn-default">
+                <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+            </button>
+            </span>
+            </div>
+            <?php ActiveForm::end() ?>
+        </li>
+    </ul>
     <?= $this->render('widgets/orders_grid', [
             'dataProvider' => $dataProvider,
             'searchModel' => $searchModel,
